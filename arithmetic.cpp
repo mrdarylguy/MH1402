3b91f5c3937a6046948982ff07dbef0f6a35e85a
Daryl Tng <42925677+mrdarylguy@users.noreply.github.com>
2022-01-10 23:30:35 +0800

    Change from gcc to g++

---
+++ arithmetic.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    x=3;
+    y=4;
+    z=x+y;
+
+    cout<<"The value of x is: "<<x<<endl;
+    cout<<"The value of y is: "<<y<<endl;
+    cout<<"The value of z is: "<<z<<endl;
+
+    return 0; 
+}
